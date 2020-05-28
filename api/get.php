b300115580b2a7acdc35a1efc0a86925396ab91c
Sandeepa-RA <sandeeparanatungavcg@gmail.com>
2020-05-28 16:46:48 +0530

    get api done

--- api/get.php
+++ api/get.php
@@ -1,15 +1,39 @@
 <?php
     header('Access-Control-Allow-Origin:*');
     header('Content-Type:application/json');
+
     require_once '../config/config.php';
     require_once '../request/requests.php';
         //instantiate DBConfig
         $database=new DBConfig();
+        $connection=$database->connect();
         //instantiate Request and send arguments to constructor
-        $request=new Request($database->connect());
-        //get results to multidimensional associative array
-        $resultArray=mysqli_fetch_all($request->getAll(),MYSQLI_ASSOC);
-        //encode array to json
-        $resultJson=json_encode($resultArray);
-        echo $resultJson;
+        $request=new Request($connection);
+        $result=$request->getAll();
+        if(mysqli_num_rows($result)){
+            $resultArray=array();
+            $resultArray['data']=array();
+            while($row=mysqli_fetch_assoc($result)){
+                //array indexes to variables
+                extract($row);
+                $resultItems=array(
+                    'id'=>$id,
+                    'title'=>$title,
+                    'body'=>$body,
+                    'autor'=>$autor,
+                    'created_at'=>$created_at
+                );
+                //add resultItems at the end of the resultArray
+                array_push($resultArray['data'],$resultItems);
+            }
+            //encode to json
+            $jsonResult=json_encode($resultArray);
+            echo $jsonResult;
+        }
+        else{
+            //no data rows
+            $error=array('message'=>'No data found!');
+            echo json_encode($error);
+        }
+
 ?>
