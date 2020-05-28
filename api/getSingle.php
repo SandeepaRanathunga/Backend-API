8adacd93826eb576250dcd324a4d0bda72fe7ced
Sandeepa-RA <sandeeparanatungavcg@gmail.com>
2020-05-29 02:09:25 +0530

    getSingle api done

---
+++ api/getSingle.php
@@ -0,0 +1,37 @@
+<?php
+    header('Access-Control-Allow-Origin:*');
+    header('Content-Type:application/json');
+
+    include '../config/config.php';
+    include '../request/requests.php';
+
+    $database=new DBConfig();
+    $connection=$database->connect();
+    $request=new Request($connection);
+    $request->id=$_GET['id'];
+    $result=$request->getSingle();
+    if(mysqli_num_rows($result)){
+        $resultArray=array();
+        $resultArray['data']=array();
+        $row=mysqli_fetch_assoc($result);
+        extract($row);
+        $resultItem=array(
+            'id'=>$id,
+            'title'=>$title,
+            'body'=>$body,
+            'autor'=>$autor,
+            'created_at'=>$created_at
+        );
+        array_push($resultArray['data'],$resultItem);
+        $jsonArray=json_encode($resultArray);
+        echo $jsonArray;
+    }
+    else{
+        $error=array('message'=>'data not found!');
+        echo json_encode($error);
+    }
+
+    
+
+
+?>
--- request/requests.php
+++ request/requests.php
@@ -2,6 +2,7 @@
     class Request{
         private $connection;
         private $table='posts';
+        public $id;
 
         public function __construct($connection ){
             $this->connection=$connection;
@@ -9,6 +10,11 @@
 
         public function getAll(){
             $query='SELECT * FROM '.$this->table;
+            $result=mysqli_query($this->connection,$query);
+            return $result;
+        }
+        public function getSingle(){
+            $query='SELECT * FROM '.$this->table.' WHERE id='.$this->id;
             $result=mysqli_query($this->connection,$query);
             return $result;
         }
